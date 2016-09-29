49ce57482648e13fac3e184e2badf0517dca2659
Noel Namai <Noel Namai>
2016-09-29 15:30:56 -0400

    update

---
+++ networkR.R
@@ -0,0 +1,10 @@
+setwd(".")
+library("networkD3")
+
+nodes = read.csv("Rnodes.csv")
+links = read.csv("Rlinks.csv")
+
+nodes$group = sample(c(1,2,3), nrow(nodes), replace=TRUE)
+
+network = forceNetwork(Links=links, Nodes=nodes, Source="source", Target="target", Value="value", NodeID="name", Group="group", opacity=10, fontSize=10, fontFamily="serif"); network
+
