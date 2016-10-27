912775aba1f1fbd1903f12a6fae18c70b3b42f87
Noel Namai <noelnamai@fas.harvard.edu>
2016-10-27 19:50:40 -0400

    update

--- network.R
+++ network.R
@@ -4,7 +4,16 @@ library("networkD3")
 nodes = read.csv("Rnodes.csv")
 links = read.csv("Rlinks.csv"); head(links, 10)
 
-nodes$group = sample(c(1,2,3), nrow(nodes), replace=TRUE)
+network = forceNetwork(Links=links, Nodes=nodes, Nodesize="degree", 
+                       Source="source", Target="target", Value="value", 
+                       NodeID="name", Group="industry", opacity=10, fontSize=10, 
+                       fontFamily="serif", charge=-500, zoom=TRUE, 
+                       radiusCalculation=JS("Math.sqrt(d.nodesize)*2.5")); network
 
-network = forceNetwork(Links=links, Nodes=nodes, Source="source", Target="target", Value="value", NodeID="name", Group="group", opacity=10, fontSize=10, fontFamily="serif"); network
+saveNetwork(network, file="network.html", selfcontained=TRUE)
 
+
+
+df = read.csv("http://www.barabasilab.com/pubs/CCNR-ALB_Publications/200705-14_PNAS-HumanDisease/Suppl/supplementary_tableS1.txt", sep="\t", skip=1, names=FALSE)
+names(df) = tolower(names(df))
+head(df)
